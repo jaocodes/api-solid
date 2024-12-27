71668401b20bd4cc447cba480083032f867c7ed2
jaomuro <jhonskaz@gmail.com>
2024-12-27 13:40:51 -0300

    adiciona validação para evitar usuários com email duplicado

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -12,6 +12,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
 	const { name, email, password } = registerBodySchema.parse(request.body);
 
+	const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
+
+	if (userWithSameEmail) {
+		return reply.status(409).send();
+	}
+
 	const password_hash = await hash(password, 6);
 
 	await prisma.user.create({ data: { name, email, password_hash } });
