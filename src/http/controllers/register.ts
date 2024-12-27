db3d6efee92d59f9005cd5aa3210b51debbd475e
jaomuro <jhonskaz@gmail.com>
2024-12-27 14:26:10 -0300

    finaliza a inversão de dependências para o service de criação de usuários

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,7 +3,7 @@ import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
 import { hash } from 'bcryptjs';
 import { RegisterUseCase } from '@/use-cases/register';
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
 	const registerBodySchema = z.object({
@@ -15,8 +15,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 	const { name, email, password } = registerBodySchema.parse(request.body);
 
 	try {
-		const prismaUsersRepository = new PrismaUsersRepository();
-		const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+		const usersRepository = new PrismaUsersRepository();
+		const registerUseCase = new RegisterUseCase(usersRepository);
 
 		await registerUseCase.execute({ name, email, password });
 	} catch (error) {
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma';
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
+import type { UsersRepository } from '@/repositories/users-repository';
 import { hash } from 'bcryptjs';
 
 interface RegisterUseCaseRequest {
@@ -9,13 +9,10 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-	constructor(private usersRepository: any) {}
+	constructor(private usersRepository: UsersRepository) {}
 
 	async execute({ name, email, password }: RegisterUseCaseRequest) {
-		const userWithSameEmail = await prisma.user.findUnique({
-			where: { email },
-		});
-
+		const userWithSameEmail = await this.usersRepository.findByEmail(email);
 		if (userWithSameEmail) {
 			throw new Error('User with same email already exists');
 		}
