33440f67fdc57fc6c95a4916ab6a0aae178df164
jaomuro <jhonskaz@gmail.com>
2024-12-27 20:56:09 -0300

    aplica TDD e Mocking para testar se os check-ins foram realizados no mesmo dia, gerando erro

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,5 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
-import { hash } from 'bcryptjs'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from './check-in'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 
@@ -10,6 +9,11 @@ describe('check-in use case', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new CheckInUseCase(checkInsRepository)
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
     })
 
     it('should be able to check in', async () => {
@@ -19,5 +23,23 @@ describe('check-in use case', () => {
         })
 
         expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to check in twice on the same day', async () => {
+        vi.setSystemTime(new Date(2024, 11, 27, 10, 0, 0))
+
+        await sut.execute({
+            gymId: 'gym-id',
+            userId: 'user-id',
+        })
+
+        vi.setSystemTime(new Date(2024, 11, 27, 20, 0, 0))
+
+        await expect(() =>
+            sut.execute({
+                gymId: 'gym-id',
+                userId: 'user-id',
+            }),
+        ).rejects.toBeInstanceOf(Error)
     })
 })
