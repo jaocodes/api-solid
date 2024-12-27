6c0607ced66866d4cf5df9e60218cbf71f079bab
jaomuro <jhonskaz@gmail.com>
2024-12-27 19:17:16 -0300

    adiciona service que retorna perfil de usuário pelo id

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -2,6 +2,11 @@ import type { User, Prisma } from '@prisma/client'
 import type { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
+    async findById(id: string) {
+        const user = this.items.find((item) => item.id === id)
+        if (!user) return null
+        return user
+    }
     public items: User[] = []
 
     async findByEmail(email: string) {
