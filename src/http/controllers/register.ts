ca0c0d12a2aeefaab54c495828f9e6dff89c3502
jaomuro <jhonskaz@gmail.com>
2024-12-27 18:44:09 -0300

    refatora adicionando factory pattern na criação de instâncias dos services

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,10 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
-import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
-import { hash } from 'bcryptjs';
-import { RegisterUseCase } from '@/use-cases/register';
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error';
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
 	const registerBodySchema = z.object({
@@ -16,9 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 	const { name, email, password } = registerBodySchema.parse(request.body);
 
 	try {
-		const usersRepository = new PrismaUsersRepository();
-		const registerUseCase = new RegisterUseCase(usersRepository);
-
+		const registerUseCase = makeRegisterUseCase();
 		await registerUseCase.execute({ name, email, password });
 	} catch (error) {
 		if (error instanceof UserAlreadyExistsError)
