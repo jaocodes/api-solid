8776837e9e3d4cc68192ff6788f194d0bdc2c4dc
jaomuro <jhonskaz@gmail.com>
2024-12-27 16:52:30 -0300

    adiciona repositório em memória para servir de base para os testes de services

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,25 @@
+import type { User, Prisma } from '@prisma/client';
+import type { UsersRepository } from '../users-repository';
+
+export class InMemoryUsersRepository implements UsersRepository {
+	public items: User[] = [];
+
+	async findByEmail(email: string) {
+		const user = this.items.find((item) => item.email === email);
+
+		if (!user) return null;
+		return user;
+	}
+	async create(data: Prisma.UserCreateInput) {
+		const user = {
+			id: 'user-1',
+			name: data.name,
+			email: data.email,
+			created_at: new Date(),
+			password_hash: data.password_hash,
+		};
+
+		this.items.push(user);
+		return user;
+	}
+}
