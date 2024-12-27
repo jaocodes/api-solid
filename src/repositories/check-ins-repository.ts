1310d9f3278e126a101bc0eb0b21cec8beca61ac
jaomuro <jhonskaz@gmail.com>
2024-12-27 19:49:06 -0300

    adiciona repositório em memória para a entidade de checkIns

---
+++ src/repositories/check-ins-repository.ts
@@ -0,0 +1,5 @@
+import type { Prisma, CheckIn } from '@prisma/client'
+
+export interface CheckInsRepository {
+    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+}
--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,13 +1,15 @@
 import type { User, Prisma } from '@prisma/client'
 import type { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
+    public items: User[] = []
+
     async findById(id: string) {
         const user = this.items.find((item) => item.id === id)
         if (!user) return null
         return user
     }
-    public items: User[] = []
 
     async findByEmail(email: string) {
         const user = this.items.find((item) => item.email === email)
@@ -17,7 +19,7 @@ export class InMemoryUsersRepository implements UsersRepository {
     }
     async create(data: Prisma.UserCreateInput) {
         const user = {
-            id: 'user-1',
+            id: randomUUID(),
             name: data.name,
             email: data.email,
             created_at: new Date(),
