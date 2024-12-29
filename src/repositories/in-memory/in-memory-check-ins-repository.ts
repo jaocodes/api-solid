f3be842bb3a100e5333098288b4253554a83a960
jaomuro <jhonskaz@gmail.com>
2024-12-29 11:10:40 -0300

    adiciona service e testes para consulta de histórico de check ins

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -33,4 +33,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
         this.items.push(checkIn)
         return checkIn
     }
+
+    async findManyByUserId(userId: string, page: number) {
+        const checkIns = this.items
+            .filter((item) => item.user_id === userId)
+            .slice((page - 1) * 20, page * 20)
+
+        return checkIns
+    }
 }
