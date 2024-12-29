2b620d2c2cfabdbdb634d7238d7186fe807d4460
jaomuro <jhonskaz@gmail.com>
2024-12-28 22:56:11 -0300

    adiciona service para criação de gyms

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,8 +1,24 @@
-import type { Gym } from '@prisma/client'
+import { type Gym, Prisma } from '@prisma/client'
 import type { GymsRepository } from '../gyms-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryGymsRepository implements GymsRepository {
     public items: Gym[] = []
+
+    async create(data: Prisma.GymCreateInput) {
+        const gym = {
+            id: randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            latitude: new Prisma.Decimal(data.latitude.toString()),
+            longitude: new Prisma.Decimal(data.longitude.toString()),
+        }
+
+        this.items.push(gym)
+
+        return gym
+    }
 
     async findById(id: string): Promise<Gym | null> {
         const gym = this.items.find((item) => item.id === id)
