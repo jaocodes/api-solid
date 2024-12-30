be02dfaf18a9f181493b270ba7946bd181c0f757
jaomuro <jhonskaz@gmail.com>
2024-12-29 22:05:00 -0300

    configuração de setup de testes adicionanco schemas de testes ao banco de dados

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,8 +4,10 @@ const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
     PORT: z.coerce.number().default(3333),
     SUPER_SECRET_JWT: z.string(),
+    DATABASE_URL: z.string(),
 })
 
+console.log(process.env)
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
