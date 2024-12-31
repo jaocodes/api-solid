921a3bc03eb5633f05878ad6b93d2a6a6192ad3e
jaomuro <jhonskaz@gmail.com>
2024-12-30 22:35:11 -0300

    adiciona controllers de check-ins

---
+++ src/http/controllers/check-ins/metrics.ts
@@ -0,0 +1,11 @@
+import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function metrics(request: FastifyRequest, reply: FastifyReply) {
+    const userMetricsUseCase = makeGetUserMetricsUseCase()
+    const { checkInsCount } = await userMetricsUseCase.execute({
+        userId: request.user.sub,
+    })
+
+    return reply.status(200).send({ checkInsCount })
+}
--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -3,12 +3,12 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 export async function search(request: FastifyRequest, reply: FastifyReply) {
-    const searchGymBodySchema = z.object({
+    const searchGymQuerychema = z.object({
         page: z.coerce.number().min(1).default(1),
         query: z.string(),
     })
 
-    const { page, query } = searchGymBodySchema.parse(request.body)
+    const { page, query } = searchGymQuerychema.parse(request.query)
 
     const searchGymsUseCase = makeSearchGymsUseCase()
     const { gyms } = await searchGymsUseCase.execute({
